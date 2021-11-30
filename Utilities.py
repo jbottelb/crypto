13005b51816a8a5a7eaeaa5027aa0435ddc50158
Bradley Budden <bbudden@student13.cse.nd.edu>
2021-11-29 20:45:58 -0500

    more message verifications done

--- Utilities.py
+++ Utilities.py
@@ -11,6 +11,7 @@ and receiving different types of messages.
 
 import socket
 import json
+from types import prepare_class
 
 from MessageTypes import MessageTypes
 
@@ -38,7 +39,7 @@ class Utilities:
     Returns: True if valid, False otherwise
     '''
     @staticmethod
-    def isValidMessage(message: dict) -> bool:
+    def _isValidMessage(message: dict) -> bool:
         type = message.get("Type", 0)
         if not type or type not in MessageTypes._value2member_map_:
             return False
@@ -55,8 +56,24 @@ class Utilities:
             # TODO:
             pass
         elif type == MessageTypes.Send_Transaction:
-            # TODO:
-            pass
+            if len(message.keys()) != 6:
+                return False
+            tid = message.get("Transaction_ID", 0)
+            if not tid or type(tid) != str:
+                return False
+            sender_key = message.get("Sender_Public_Key", 0)
+            if not sender_key or type(sender_key) != str:
+                return False
+            recipient_key = message.get("Recipient_Public_Key", 0)
+            if not recipient_key or type(recipient_key) != str:
+                return False
+            amount = message.get("Amount", 0)
+            if not amount or type(amount) != int:
+                return False
+            signature = message.get("Signature", 0)
+            if not signature or type(signature) != str:
+                return False
+            return True
         elif type == MessageTypes.Send_Block:
             # TODO:
             pass
@@ -90,24 +107,66 @@ class Utilities:
             return True
 
         elif type == MessageTypes.Get_Miner_Count:
-            # TODO:
-            pass
+            if len(message.keys()) != 1:
+                return False
+            return True
 
         elif type == MessageTypes.Join_As_Miner:
-            # TODO:
-            pass
+            if len(message.keys()) != 2:
+                return False
+            searching_for_owner = message.get("Searching_For_Owner", -1)
+            if searching_for_owner == -1:
+                return False
+            if type(searching_for_owner) != str:
+                return False
+            return True 
 
         elif type == MessageTypes.Get_Miner_Count_Response:
-            # TODO:
-            pass
+            if len(message.keys()) != 2:
+                return False
+            count = message.get("Count", -1)
+            if count == -1:
+                return False
+            if type(count) != int or count < 0:
+                return False
+            return True
     
         elif type == MessageTypes.Join_As_Miner_Response:
-            # TODO:
-            pass
+            if len(message.keys()) != 2:
+                return False
+            full_node_to_try = message.get("Full_Node_To_Try", 0)
+            if not full_node_to_try or type(full_node_to_try) != list:
+                return False
+            if len(full_node_to_try) != 2:
+                return False
+            if type(full_node_to_try[0]) != str or type(full_node_to_try[1]) != int:
+                return False
+            return True
+
+        elif type == MessageTypes.Start_New_Block:
+            if len(message.keys()) != 4:
+                return False
+            transactions = message.get("Transactions", 0)
+            if not transactions or type(transactions) != list:
+                return False
+            for item in transactions:
+                if type(item) != dict:
+                    return False
+            previous_hash = message.get("Previous_Hash", 0)
+            if not previous_hash or previous_hash != str:
+                return False
+            block_index = message.get("Block_Index", 0)
+            if not block_index or type(block_index) != int or block_index < 0:
+                return False
+            return True
+
+        elif type == MessageTypes.Start_New_Block_Response:
+            if len(message.keys()) != 1:
+                return False
+            return True
 
 
         # TODO: check other types
-
 
 
         return False
@@ -123,7 +182,7 @@ class Utilities:
             data = str(data, 'utf-8') # convert to string
             message = json.loads(data)
             # check that message is a valid message form
-            if Utilities.isValidMessage(message):
+            if Utilities._isValidMessage(message):
                 return message
         except Exception as e:
             # improperly formatted message
