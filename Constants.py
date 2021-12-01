0816814dfa5f72a2884eabf55018bd0ab402eaac
Bradley Budden <bbudden@student13.cse.nd.edu>
2021-11-30 19:51:04 -0500

    increase catalog update interval

--- Constants.py
+++ Constants.py
@@ -2,7 +2,7 @@ class Constants():
     # We use a large buffer size of 256 Kb so that
     # we can handle sending full blockchains
     BUF_SIZE = 262144
-    CATALOG_UPDATE_INTERVAL = 10
+    CATALOG_UPDATE_INTERVAL = 60
     CATALOG_ENDPOINT = 'catalog.cse.nd.edu.'
     CATALOG_PORT = 9097
     PROJECT_NAME = "JBnetwork"
